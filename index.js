ffc2dec844254314af32451cebb665d4627f27fd
Malena de Juan <malena.dj4@gmail.com>
2021-03-31 13:44:39 +0200

    consulta el la linea elegida y coge su color para el texto

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ const { program } = require("commander");
 const preguntas = require("./preguntas");
 let linea;
 let texto;
+const url = "https://api.tmb.cat/v1/transit/linies/metro/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da"
+const lineasMetro = require("./lineasMetro.json");
 
 inquirer.prompt(preguntas).then(respuestas => {
   if (respuestas.transporte === "Bus") {
     console.log(chalk.yellow("No hay información disponible sobre autobuses."));
     process.exit(0);
-  }
-  if (respuestas.errores === true) {
-    texto = chalk.red.bold("La línea no existe");
-  }
-  if (respuestas.linea) {
-    if (color) {
-      console.log(chalk.hex(color)(`Línea ${respuestas.linea}.`))
-    } else {
-      /* que sea del color de la linea de metro */
-    }
   } else {
-    console.log(texto);
-    process.exit(0);
+    if (respuestas.errores === true) {
+      texto = chalk.red.bold("La línea no existe");
+    }
+    if (respuestas.linea) {
+      const lineaElegida = lineasMetro.features.find(linia => linia.properties.NOM_LINIA === respuestas.linea);
+      if (lineaElegida) {
+        if (color) {
+          console.log(chalk.hex(color)(`Línea ${respuestas.linea}.`))
+        } else {
+          let colorLinea = "#" + lineaElegida.properties.COLOR_LINIA;
+          console.log(chalk.hex(colorLinea)(lineaElegida.properties.NOM_LINIA));
+        }
+      }
+    } else {
+      console.log(texto);
+      process.exit(0);
+    }
   }
-})
+});
 
 program
   .option("-c, --color <color>", "Para introducir un color en hexadecimal")
