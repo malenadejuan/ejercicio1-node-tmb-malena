e23f4e42a749e678c2fc7dcf382a82ae27b816dc
Malena de Juan <malena.dj4@gmail.com>
2021-03-31 13:57:14 +0200

    añadido json prueba paradas linea

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { program } = require("commander");
 const preguntas = require("./preguntas");
 let linea;
 let texto;
-const url = "https://api.tmb.cat/v1/transit/linies/metro/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da"
+const url = "https://api.tmb.cat/v1/transit/linies/metro/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da";
+const urlParadas = "https://api.tmb.cat/v1/transit/linies/metro/101/estacions/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da"
 const lineasMetro = require("./lineasMetro.json");
+const paradas = require("./paradas.json");
 
 inquirer.prompt(preguntas).then(respuestas => {
   if (respuestas.transporte === "Bus") {
@@ -28,6 +30,7 @@ inquirer.prompt(preguntas).then(respuestas => {
           colorTexto = "#" + lineaElegida.properties.COLOR_LINIA;
         }
         console.log(chalk.hex(colorTexto)(`Línea ${lineaElegida.properties.NOM_LINIA}. ${lineaElegida.properties.DESC_LINIA}`))
+        /* Pedir todas las paradas de la linea */
       } else {
         console.log(texto);
         process.exit(0);
