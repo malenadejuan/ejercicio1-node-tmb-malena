cfa32e098236d57cd05c4d07d86e919a4f98fd3b
Malena de Juan <malena.dj4@gmail.com>
2021-03-31 20:34:46 +0200

    importado dotenv y creado .env + llamada 1a api

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const inquirer = require("inquirer");
 const fetch = require("node-fetch");
 const chalk = require("chalk");
+require("dotenv").config();
 const { program } = require("commander");
 const preguntas = require("./preguntas");
 let linea;
-const url = "https://api.tmb.cat/v1/transit/linies/metro/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da";
+/* const url = "https://api.tmb.cat/v1/transit/linies/metro/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da"; */
 const urlParadas = "https://api.tmb.cat/v1/transit/linies/metro/101/estacions/?app_id=23f83909&app_key=09c9602ce5c6bb10b89b5c5c6f6849da";
-const lineasMetro = require("./lineasMetro.json");
+
+
 const paradas = require("./paradas.json");
 
 inquirer.prompt(preguntas).then(respuestas => {
@@ -15,37 +17,41 @@ inquirer.prompt(preguntas).then(respuestas => {
     process.exit(0);
   } else {
     if (respuestas.linea) {
-      const lineaElegida = lineasMetro.features.find(linia => linia.properties.NOM_LINIA === respuestas.linea);
-      if (lineaElegida) {
-        let texto = "";
-        let colorTexto;
-        let informacionLinea = paradas.features.map(parada => ({
-          nombre: abrev ? parada.properties.NOM_ESTACIO.slice(0, 3) + "." : parada.properties.NOM_ESTACIO,
-          coordenadas: parada.geometry.coordinates,
-          fecha: parada.properties.DATA_INAUGURACIO
-        }
-        ));
-        if (color) {
-          colorTexto = color;
-        } else {
-          colorTexto = "#" + lineaElegida.properties.COLOR_LINIA;
-        }
-        informacionLinea.forEach(parada => {
-          texto = texto + " " + JSON.stringify(parada.nombre);
-          if (respuestas.informacion.includes("Coordenadas")) {
-            texto = texto + " " + JSON.stringify(parada.coordenadas);
-          }
-          if (respuestas.informacion.includes("Fecha de inauguración")) {
-            texto = texto + " " + JSON.stringify(parada.fecha);
+      fetch(`${process.env.API_URL_LINEA}/?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`)
+        .then(resp => resp.json())
+        .then(datos => {
+          const lineaElegida = datos.features.find(linia => linia.properties.NOM_LINIA === respuestas.linea);
+          if (lineaElegida) {
+            let texto = "";
+            let colorTexto;
+            let informacionLinea = paradas.features.map(parada => ({
+              nombre: abrev ? parada.properties.NOM_ESTACIO.slice(0, 3) + "." : parada.properties.NOM_ESTACIO,
+              coordenadas: parada.geometry.coordinates,
+              fecha: parada.properties.DATA_INAUGURACIO
+            }
+            ));
+            if (color) {
+              colorTexto = color;
+            } else {
+              colorTexto = "#" + lineaElegida.properties.COLOR_LINIA;
+            }
+            informacionLinea.forEach(parada => {
+              texto = texto + " " + JSON.stringify(parada.nombre);
+              if (respuestas.informacion.includes("Coordenadas")) {
+                texto = texto + " " + JSON.stringify(parada.coordenadas);
+              }
+              if (respuestas.informacion.includes("Fecha de inauguración")) {
+                texto = texto + " " + JSON.stringify(parada.fecha);
+              }
+            });
+            console.log(chalk.hex(colorTexto)("Línea " + lineaElegida.properties.NOM_LINIA + ". " + lineaElegida.properties.DESC_LINIA + texto));
+          } else {
+            if (respuestas.errores === true) {
+              console.log(chalk.red.bold("La línea no existe"));
+            }
+            process.exit(0);
           }
         });
-        console.log(chalk.hex(colorTexto)("Línea " + lineaElegida.properties.NOM_LINIA + ". " + lineaElegida.properties.DESC_LINIA + texto));
-      } else {
-        if (respuestas.errores === true) {
-          console.log(chalk.red.bold("La línea no existe"));
-        }
-        process.exit(0);
-      }
     }
   }
 });
